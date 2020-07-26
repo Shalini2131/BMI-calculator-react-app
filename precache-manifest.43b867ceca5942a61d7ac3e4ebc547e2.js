02f652efc6f12d3a8e4ade5819dffc7bd365a1ed
Shalini2131 <shalukallepalli21@gmail.com>
2020-07-26 19:10:37 +0530

    Updates

---
+++ precache-manifest.43b867ceca5942a61d7ac3e4ebc547e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2630f949728ace208b36795e796235",
+    "url": "/BMI-calculator-react-app/index.html"
+  },
+  {
+    "revision": "1c67d9ae7f6ff21d5e62",
+    "url": "/BMI-calculator-react-app/static/css/main.ffde277c.chunk.css"
+  },
+  {
+    "revision": "f8c65a1ec61be4ab27c3",
+    "url": "/BMI-calculator-react-app/static/js/2.24dca068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BMI-calculator-react-app/static/js/2.24dca068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c67d9ae7f6ff21d5e62",
+    "url": "/BMI-calculator-react-app/static/js/main.66c5c904.chunk.js"
+  },
+  {
+    "revision": "a35aee6ec26499d9b987",
+    "url": "/BMI-calculator-react-app/static/js/runtime-main.e68177b4.js"
+  }
+]);
